dbd51a9d07fd2fb0ff12b41b3de7c5718fc6e0da
Aakash <aakashmohole@gmail.com>
2025-03-19 23:29:13 +0530

    loyaltypoints done

--- backend/u_store/urls.py
+++ backend/u_store/urls.py
@@ -22,4 +22,5 @@ urlpatterns = [
     path('api/user/', include('authentication.urls')),  # ✅ Corrected
     path('api/', include('products.urls')),
     path('api/', include('orders.urls')),
+    path('api/loyalty/', include('loyalty.urls')),
 ]
